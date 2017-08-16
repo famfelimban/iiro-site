a4dac5c9f19a66af93ccf198a7f9f286fa1060b1
famfelimban <famfelimban@outlook.sa>
2017-08-16 09:15:19 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<title>إغاثة | Egatha</title>
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+
+<scroll="no" style="overflow: hidden">
+</head>
+<!--scroll="no" style="overflow: hidden"-->
+
+
+<body id="welcome-page">
+
+
+		<div class="lan-select">
+			<a href="Ar/index.php"><div class="lan">العربية</div></a>
+			<a href="En/index.php"><div class="lan">English</div></a>
+			<a href="index.php"><div class="lan">Français</div></a>
+			
+		</div>
+		<div class="welcome-page-logo">
+		<img src="test.png">
+		</div>
+
+
+
+
+</body>
+</html>
